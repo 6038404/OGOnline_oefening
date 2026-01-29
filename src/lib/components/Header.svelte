96297078ddf7fac9776aa3e99524fdb1c6f14e16
Luc Martijn <martijnluc093@gmail.com>
2026-01-29 11:42:30 +0100

    Dynamisch import header & Footer + Nieuwe Route

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  export let title: string = "Mijn App";
+</script>
+
+<header>
+  <h1>{title}</h1>
+  <nav>
+    <a href="/Home">Home</a>
+    <a href="/Home">About</a>
+  </nav>
+</header>
---
+++ src/routes/Home/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import Header from '$lib/components/Header.svelte';
+  import Footer from '$lib/components/Footer.svelte';
+</script>
+
+<Header title="Mijn Professionele App" />
+
+<main class="p-4">
+  <slot />  <!-- hier komt de content van de pagina -->
+</main>
+
+<Footer />
